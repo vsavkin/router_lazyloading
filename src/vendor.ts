315c23cf9c7d18c9af265374cd2102c5b86b2741
Victor Savkin <avix1000@gmail.com>
2017-01-16 17:15:21 -0500

    extracts vendor chunk

---
+++ src/vendor.ts
@@ -0,0 +1,4 @@
+import '@angular/core';
+import '@angular/forms';
+import '@angular/router';
+import '@angular/platform-browser';
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
 const path = require('path');
 const ngtools = require('@ngtools/webpack');
+const webpack = require("webpack");
+const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 
 module.exports = {
+  devtool: 'sourcemap',
   resolve: {
     extensions: ['.ts', '.js']
   },
-  entry: './src/main.ts',
+  entry: {
+    main: './src/main.ts',
+    vendor: './src/vendor.ts'
+  },
   output: {
     path: path.join(process.cwd(), 'dist'),
     publicPath: 'dist/',
-    filename: "bundle.js"
+    filename: "[name].js"
   },
   plugins: [
     new ngtools.AotPlugin({
       tsConfigPath: './tsconfig.json',
       mainPath: "./src/main.ts"
+    }),
+    new CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new CommonsChunkPlugin({
+      async: true
     })
   ],
   module: {
@@ -22,7 +34,7 @@ module.exports = {
 		{
         test: /\.ts$/,
         loader: '@ngtools/webpack'
-      },
+    },
 	  {
         test: /\.css$/,
         loader: 'style-loader'
